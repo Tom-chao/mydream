4d97cc318bdd4a04cf797ad1541ba2d780e91f33
jiachenghao <122787987@qq.com>
2021-10-18 12:01:53 +0800

    tj

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <template>
-  <div></div>
+  <div>
+     <Header></Header>
+     <!-- 路由组件出口的地方:展示的地方 -->
+     <router-view></router-view>
+     <!-- 显示:首页、搜索显示  隐藏：登录、注册 -->
+     <!--v-show="$route.path=='/home'||$route.path=='/search'"-->
+     <Footer v-show="$route.meta.show"></Footer>
+  </div>
 </template>
 
 <script>
+//引入非路由组件
+import Header from './components/Header';
+import Footer from './components/Footer';
 export default {
   name: '',
+  components:{
+    Header,
+    Footer
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-
-
-let a = 100;
+//引入路由
+import router from '@/router';
 //创建Vue类的一个实例
 new Vue({
   render: h => h(App),
+  //注册路由,给组件的身上添加了$router与$route两个属性
+  router
 }).$mount('#app')
