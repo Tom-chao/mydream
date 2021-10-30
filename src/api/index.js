7987e683765ade20ef2c208a9ff19ecf2724fbe5
jiachenghao <122787987@qq.com>
2021-10-30 15:43:57 +0800

    day8

--- src/api/index.js
+++ src/api/index.js
@@ -9,9 +9,11 @@ export const reqCategoryList = ()=>request({url:'/product/getBaseCategoryList',m
 export const reqGetBannerList = ()=>mockRequest({url:'/banner',method:'get'});
 //获取Floor(楼层)数据接口的函数
 export const reqGetFloorList = ()=>mockRequest({url:`/floor`,method:'get'});
-
 //搜索产品的接口[真实的接口]
 //URL:/api/list   method:post    参数：需要携带
 //这个接口【携带参数：最多十个，十个属性可以传递，也可以不传递，但是至少给给服务器携带一个空对象】
-export const reqSearchList = (data)=>request({url:"/list",method:'post',data})
+export const reqSearchList = (data)=>request({url:"/list",method:'post',data});
+//获取产品详情的接口
+///api/item/{skuId}  get
+export const reqDetailList = (skuId)=>request({url:`/item/${skuId}`,method:'get'});
 
